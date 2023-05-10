1eb271e8dda8f28585b97f1c6eb857d093241014
Piraya99 <66905763+Piraya99@users.noreply.github.com>
2023-05-10 19:32:13 +0200

    Made my rocket!2

--- js/rocket.js
+++ js/rocket.js
@@ -2,30 +2,33 @@ var timer = null;
 var countdownNumber = 10;
 
 var changeState = function (state) {
-  document.body.className = 'body-state' + state;
+  document.body.className = 'body-state'+state;
   clearInterval(timer);
   countdownNumber = 10;
-  document.getElementById('countdown').innerHTML = countdownNumber;
 
-  if (state == 2) {
-    timer = setInterval (function () {
-      countdownNumber = countdownNumber - 1;
-      document.getElementById('countdown').innerHTML = countdownNumber;
-      if (countdownNumber <= 0) {
+  if (state == 2){
+    timer = setInterval(function(){
+      countdownNumber = countdownNumber-1;
+      document.getElementById('countdown').innerHTML = countdownNumber; 
+      
+      if (countdownNumber <=0){
         changeState(3);
       };
-    }, 500);
+    },500);
   } else if (state == 3) {
-    var success = setTimeout(function () {
-      var randomNumber = Math.round(Math.random() * 10);
+    var success = setTimeout(function ()
+      {
+        var randomNumber = Math.round(Math.random()*10);
 
-      console.log('randomNumber: ', randomNumber);
+        console.log('randomNumber:'+randomNumber)
 
-      if(randomNumber > 5) {
-        changeState(4);
-      } else {
-        changeState(5);
-      }
-    }, 2000)
-  }
-};
+        if (randomNumber > 5) {
+          changeState(4);
+
+        } else {
+          changeState(5);
+        }
+
+    },2000);
+  };
+}
